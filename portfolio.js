e4f8153a9511e09b4196272eba7f291522e003a7
junipera <juniper.alcorn@gmail.com>
2019-05-21 11:52:06 -0400

    clean up code

--- portfolio.js
+++ portfolio.js
@@ -3,38 +3,8 @@ const about = document.getElementById('about')
 const portfolio = document.getElementById('portfolio')
 const contact = document.getElementById('contact')
 const header = document.getElementById('headerScroll')
-//buttons
-const aboutButton = document.getElementById('aboutButton')
-const portfolioButton = document.getElementById('portfolioButton')
-const contactButton = document.getElementById('contactButton')
-//other variables
 
 //navigation functions
-let goHome = function(){
-    aboutButton.style.display='none';
-    portfolioButton.style.display='flex';
-    // contactButton.style.display='flex';
-    about.style.display='block';
-    portfolio.style.display='none';
-    // contact.style.display='none'
-}
-let goPortfolio = function(){
-    aboutButton.style.display='flex';
-    portfolioButton.style.display='none';
-    // contactButton.style.display='flex';
-    about.style.display='none';
-    portfolio.style.display='flex';
-    // contact.style.display='none'
-}
-let goContact = function(){
-    aboutButton.style.display='flex';
-    portfolioButton.style.display='flex';
-    // contactButton.style.display='none';
-    about.style.display='none';
-    portfolio.style.display='none';
-    // contact.style.display='flex'
-}
-
 function findOffset(element) {
     let top = 300, left = 0;
   
